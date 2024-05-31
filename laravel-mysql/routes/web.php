1eb05a10222b54c18f2116e7474e0140e1123c84
phamvinhbkav <phamvinhcntt@gmail.com>
2024-05-31 08:31:33 +0700

    vincent_mai lavarel source code

---
+++ laravel-mysql/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'hello world';
+});
+
+Route::get('/k8s', function () {
+    return env('K8S_MESSAGE', 'K8S_MESSAGE is not set');
+});
